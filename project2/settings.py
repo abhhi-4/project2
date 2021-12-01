95d695af3ce17bfa72761750201fce10f9a07a4d
abhhi-4 <abhishekcabc@gmail.com>
2021-12-01 10:12:59 +0000

    version4

--- project2/settings.py
+++ project2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b#wywo$)sv3#xbo0ugq3+ab-a4vzju)3rw*opq+!_j)fi3r2a9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
